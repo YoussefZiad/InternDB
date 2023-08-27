3210f1bfcd16345c7145e2953ab73be3baeb6785
YoussefZiad <86000683+YoussefZiad@users.noreply.github.com>
2023-08-27 12:33:33 +0300

    Implemented delete feature: delete intern

--- src/main/java/database/DBLayer.java
+++ src/main/java/database/DBLayer.java
@@ -64,7 +64,10 @@ public class DBLayer {
     }
 
     public void deleteInternFromDB(int id) throws SQLException {
-
+        Session session = startSession();
+        Query query = session.createQuery("delete from Intern where id = :id");
+        query.setParameter("id", id);
+        commitTransaction(session);
     }
 
     public void updateInternAcceptanceInDB(int id, boolean accept) throws SQLException {
